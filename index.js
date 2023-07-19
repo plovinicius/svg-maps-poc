5f6971a41aeb304633635b3f0e0812d781f73873
Paulo Pinheiro <paulo.pinheiro@dentsu.com>
2023-07-19 12:31:45 -0300

    feat: adding example for disabled and custom tooltips

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ import './node_modules/jsvectormap/dist/js/jsvectormap.js'
 import "./node_modules/jsvectormap/dist/maps/world.js";
 import './maps/us-aea-en.js'
 import './maps/canada.js'
+import './maps/world_merc.js'
 
 console.log('init');
+
+const customTooltips = {
+    CA: `
+        <h5>Canada</h5>
+        <p>Culligan of Canada ULC</p>
+        <p>Calgary, Alberta T2P 4K9</p>
+        <p><strong>Phone:</strong> 1-800-268-5458</p>
+        <p><strong>Fax:</strong> 1-847-430-2735</p>
+        <p><strong>Website:</strong> <a href="www.culligan.com">www.culligan.com</a></p>
+        <p><strong>Email:</strong> <a mailto="internationalinfo@culligan.com">internationalinfo@culligan.com</a></p>
+    `
+}
+
+const disabledRegions = ['GL']
 
 const getOptions = (selector, map) => {
     const options = {
@@ -12,8 +27,9 @@ const getOptions = (selector, map) => {
         zoomButtons: false,
         zoomOnScroll: false,
         showTooltip: true,
-        regionsSelectable: true,
-        regionsSelectableOne: true,
+        regionsSelectable: false,
+        regionsSelectableOne: false,
+        selectedRegions: disabledRegions,
         regionStyle: {
             initial: {
                 stroke: "#ffffff",
@@ -22,20 +38,24 @@ const getOptions = (selector, map) => {
                 fillOpacity: 1,
             },
             hover: { fill: '#34dc34' },
-            selected: { fill: '#34dc34' }
+            selected: { fill: '#ebeced' },
+            selectedHover: {
+                fill: '#ebeced',
+                fillOpacity: 1,
+                cursor: "default"
+            }
         },
         // onRegionSelected: function (index, isSelected, selectedRegions) {
         //     console.log(index, isSelected, selectedRegions);
         // },
         onRegionTooltipShow(event, tooltip, code) {
             // Or you can disable tooltip for certain regions
-            if (code === 'RU') {
+            if (disabledRegions.includes(code)) {
                 tooltip.hide()
-            } else {
-                // tooltip customize
+            } else if (code === 'CA') {
+                // Tooltip customize
                 tooltip.text(
-                    `<h5>${tooltip.text()} - Country</h5>` +
-                    `<p class="text-xs">This message is gonna appear when hovering over every single reion.</p>`,
+                    customTooltips.CA,
                     true // Enables HTML
                 )
             }
@@ -51,5 +71,5 @@ const getOptions = (selector, map) => {
 const maps = {
     us: new jsVectorMap(getOptions("#us", "us_aea_en")),
     canada: new jsVectorMap(getOptions("#canada", "canada")),
-    world: new jsVectorMap(getOptions("#world", "world"))
+    world: new jsVectorMap(getOptions("#world", "world_merc"))
 }
